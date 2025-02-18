b6268ac93309a37ec5871456632f9184e7e7cd1e
agilramadann <agilramadann@gmail.com>
2025-02-18 09:24:26 +0700

    menyelsaikan tugas 1

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=sed, initial-scale=1.0">
+    <title>latihan PHP</title>
+</head>
+<body>
+    <h1><?php echo "Hello World!!"; ?></h1>
+    <p>Halo, Nama saya <?php echo "Agil Ramadhan"; ?></p>
+</body>
+</html>
---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nama = "Agil Ramadhan";
+    $email = "agilramadann@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Fakultas Teknik Unpas";
+
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+
+    echo "<p>Halo nama Saya $nama saya adalah seorang $pekerjaan di $universitas dan jurusan saya $jurusan</p>";
+
+?>
